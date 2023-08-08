4f63323a3f46232e2721e67dc0ffad3d7370801b
aziz56 <aziz.abdulaziz99@gmail.com>
2023-08-09 02:18:34 +0700

    azizmalem

---
+++ belajardirumah/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public void PrintInfo()
+    {
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Age: {Age}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person
+        {
+            Name = "John Doe",
+            Age = 30
+        };
+
+        person.PrintInfo();
+    }
+}
